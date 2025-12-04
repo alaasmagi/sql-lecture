6948179b423327c3ee608703541ccf568f593f9c
Aleksander Laasmägi <alaasmagi@gmail.com>
2025-12-04 17:13:24 +0200

    TODO: Fix Dates and UpdatedBy

--- Enitites/BaseEntity.cs
+++ Enitites/BaseEntity.cs
@@ -3,8 +3,8 @@ namespace Domain;
 public abstract class BaseEntity
 {
     public Guid Id { get; set; } = Guid.NewGuid();
-    public string CreatedBy { get; set; } = string.Empty;
+    public string CreatedBy { get; set; } = "ois v0.1";
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-    public string UpdatedBy { get; set; } = string.Empty;
+    public string UpdatedBy { get; set; } = "ois v0.1";
     public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
 }
